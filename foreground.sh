8ce7871e360c5b893c4d5796fd7500ada9664c66
francesco <info.francescobianco@gmail.com>
2019-04-24 18:15:30 +0200

    fix container host new instance updates

---
+++ foreground.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "---[ Cronicle ]---"
+
+STORAGE_CLI=/opt/cronicle/bin/storage-cli.js
+SERVERS_INFO=$(${STORAGE_CLI} get global/servers/0 2> /dev/null)
+
+if [[ -n "${SERVERS_INFO}" ]]; then
+    CONTAINER_HOST=$(node -e 'console.log(require("os").hostname().toLowerCase());')
+    CONTAINER_IP=$(awk 'END{print $1}' /etc/hosts)
+
+    echo "Update current HOST and IP to: ${CONTAINER_HOST} (${CONTAINER_IP})"
+    ${STORAGE_CLI} get global/server_groups/0 | sed -e "s/\"regexp\": \"^.*\"/\"regexp\": \"^(${CONTAINER_HOST})$\"/" | ${STORAGE_CLI} put global/server_groups/0
+    ${STORAGE_CLI} get global/servers/0 | sed -e "s/\"ip\": \".*\"/\"ip\": \"${CONTAINER_IP}\"/" -e "s/\"hostname\": \".*\"/\"hostname\": \"${CONTAINER_HOST}\"/" | ${STORAGE_CLI} put global/servers/0
+
+    [[ -f /opt/cronicle/logs/cronicled.pid ]] && rm /opt/cronicle/logs/cronicled.pid
+
+    /opt/cronicle/bin/control.sh stop > /dev/null
+fi
+
+sh /entrypoint.sh
