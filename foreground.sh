d0d927dc08c149bc5f3bc62fe1bcf07cf3497114
Francesco Bianco <info.francescobianco@gmail.com>
2019-04-24 20:14:40 +0200

    minor fix

--- foreground.sh
+++ foreground.sh
@@ -6,8 +6,8 @@ STORAGE_CLI=/opt/cronicle/bin/storage-cli.js
 SERVERS_INFO=$(${STORAGE_CLI} get global/servers/0 2> /dev/null)
 
 if [[ -n "${SERVERS_INFO}" ]]; then
-    CONTAINER_HOST=$(node -e 'console.log(require("os").hostname().toLowerCase());')
     CONTAINER_IP=$(awk 'END{print $1}' /etc/hosts)
+    CONTAINER_HOST=$(node -e 'console.log(require("os").hostname().toLowerCase());')
 
     echo "Update current HOST and IP to: ${CONTAINER_HOST} (${CONTAINER_IP})"
     ${STORAGE_CLI} get global/server_groups/0 | sed -e "s/\"regexp\": \"^.*\"/\"regexp\": \"^(${CONTAINER_HOST})$\"/" | ${STORAGE_CLI} put global/server_groups/0
